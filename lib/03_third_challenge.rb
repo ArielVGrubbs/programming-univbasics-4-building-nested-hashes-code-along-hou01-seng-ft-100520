eb9402ec7262e7672f79abf5bb6c0978d1fd4941
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-07 01:36:18 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,11 +14,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
       },
-      :hero_friends => [
-        {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
+      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"
         },
         {
           :name => "Mercutio",
